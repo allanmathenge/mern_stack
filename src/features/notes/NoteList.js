34352ff973739f4942cab9e0d6b9ec6aca7f36ca
allan <allanmathenge2@gmail.com>
2023-08-06 14:22:46 +0300

    Notes and users

--- src/features/notes/NoteList.js
+++ src/features/notes/NoteList.js
@@ -1,7 +1,49 @@
+import { useGetNotesQuery } from "./notesApiSlice"
+import Note from "./Note"
+
 const NoteList = () => {
-  return (
-    <h1>NoteList</h1>
-  )
+  const {
+    data: notes,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetNotesQuery()
+
+  let content
+
+  if (isLoading) content = <p>Loading...</p>
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>
+  }
+
+  if (isSuccess) {
+    const { ids } = notes
+
+
+      const tableContent = ids.length
+      ? ids.map(noteId => <Note key={noteId} noteId={noteId} />)
+      : null
+
+    return (
+      <table className="table table--notes">
+        <thead className="table__head">
+          <tr>
+            <th scope="col" className="table__th note__status">Username</th>
+            <th scope="col" className="table__th note__created">Created</th>
+            <th scope="col" className="table__th noted__updated">Updated</th>
+            <th scope="col" className="table__th note__title">Title</th>
+            <th scope="col" className="table__th note__username">Owner</th>
+            <th scope="col" className="table__th note__edit">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableContent}
+        </tbody>
+      </table>
+    )
+  }
 }
 
 export default NoteList
