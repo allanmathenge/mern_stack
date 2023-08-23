6e6c32e5a3b3216ec1a0819ec0a6553315a02ada
allan <allanmathenge2@gmail.com>
2023-08-23 08:34:28 +0300

    removed consoles

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3500
 
-console.log(process.env.NODE_ENV)
-
 connectDB()
 
 app.use(logger)
@@ -46,11 +44,9 @@ app.all('*', (req, res) => {
 app.use(errorHandler)
 
 mongoose.connection.once('open', () => {
-    console.log('Connected to mongoDB')
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+    app.listen(PORT)
 })
 
 mongoose.connection.on('error', err => {
-    console.log(err)
     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
