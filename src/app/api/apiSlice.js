060cceca29ac99b3ecf9b51fd4c5b86d9f81f953
allan <allanmathenge2@gmail.com>
2023-08-23 08:10:43 +0300

    CORS fix

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://allan-api.onrender.com',
+    baseUrl: 'https://allan-api-mlkp.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
